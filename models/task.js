7f7eb91708d4513b10b0a66d0e8df87584aea35b
aniebietafia <aniebietafia87@gmail.com>
2022-12-15 21:39:05 +0100

    Added Task Schema functionality

---
+++ models/task.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const TaskSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Kindly provide a name"],
+    trim: true, //Trims white spaces in the string
+    maxlength: [20, "Name cannot be more than 20 characters"],
+  },
+  completed: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+});
+
+const taskModel = mongoose.model("Task", TaskSchema);
+
+module.exports = taskModel;
