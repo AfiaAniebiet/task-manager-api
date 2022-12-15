12eeaaa603576a54e20256171485adfbdf228078
aniebietafia <aniebietafia87@gmail.com>
2022-12-15 21:41:05 +0100

    Updated the app.js

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require("express");
-
 const app = express();
+const { MONGODB_CONNECTION } = require("./db/connect");
+require("dotenv").config();
 
-const PORT = 3000;
+const taskRoute = require("./routes/tasks");
 
-app.listen(PORT, () => {
-  console.log(`Server is up and running on Port: ${PORT}!`);
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/hello", (req, res) => {
+  res.send("Task manager App");
 });
+
+app.use("/api/v1/tasks", taskRoute);
+
+const port = 3000;
+
+const start = async () => {
+  try {
+    MONGODB_CONNECTION(process.env.MONGODB_URI);
+    app.listen(port, console.log(`Server listening on Port: ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
