3c5019031e3251d51b9e31d834ae23a42121a9e9
aniebietafia <aniebietafia87@gmail.com>
2022-12-17 18:58:42 +0100

    Added basic authentication

--- models/task.js
+++ models/task.js
@@ -5,7 +5,7 @@ const TaskSchema = new mongoose.Schema({
     type: String,
     required: [true, "Kindly provide a name"],
     trim: true, //Trims white spaces in the string
-    maxlength: [20, "Name cannot be more than 20 characters"],
+    maxlength: [50, "Name cannot be more than 20 characters"],
   },
   completed: {
     type: Boolean,
